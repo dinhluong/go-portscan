5aabca8c3ba8defcb06ed0207bf75c43e7df086f
dinhluong <mrdinhluong@gmail.com>
2019-04-15 17:44:01 +0900

    Add scan function and check maxthreads

--- main.go
+++ main.go
@@ -18,10 +18,60 @@ type PortScanners struc{
 	lock *semaphore.Weighted
 }
 
-// Ulimit do ...
-func Ulimit(){
+// Ulimit does check max threads in OS...
+func Ulimit() int64 {
+	out, err := exec.Command("ulimit", "-n").Output()
+	if err != nil {
+		panic(err)
+	}
 	
+	s := strings.TrimSpace(string(out))
+	
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	
+	return i
 }
-func main(){
 
+func ScanPort(ip string, port int, timeout time.Duration) {
+	target := fmt.Sprintf("%s:%d", ip, port)
+	conn, err := net.DialTimeout("tcp", target, timeout)
+
+	if err != nil {
+		if strings.Contains(err.Error(), "too many open files") {
+			time.Sleep(timeout)
+			ScanPort(ip, port, timeout)
+		} else {
+			fmt.Println(port, "closed")
+		}
+		return
+	}
+
+	conn.Close()
+	fmt.Println(port, "open")
+}
+
+func (ps *PortScanner) Start(f, l int, timeout time.Duration) {
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+
+	for port := f; port <= l; port++ {
+		ps.lock.Acquire(context.TODO(), 1)
+		wg.Add(1)
+		go func(port int) {
+			defer ps.lock.Release(1)
+			defer wg.Done()
+			ScanPort(ps.ip, port, timeout)
+		}(port)
+	}
+}
+
+func main() {
+	ps := &PortScanner{
+		ip:   "127.0.0.1",
+		lock: semaphore.NewWeighted(Ulimit()),
+	}
+	ps.Start(1, 65535, 500*time.Millisecond)
 }
